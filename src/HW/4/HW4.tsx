b569b106084ec9a161c2ff8c2c7e713a54274056
BeautyVika <vikachk54@gmail.com>
2023-08-23 15:45:06 +0300

    added HW4

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import S from './HW4.module.css';
+import s from './HW4.module.css';
 import { Button } from './Button';
 import { Input } from './Input';
 
@@ -39,7 +39,7 @@ export const HW4 = () => {
       <ol id={'hw04-tasks'}>
         {texts.map((el, index) => {
           return (
-            <li key={index} id={`hw04-task-${index}`} className={index % 2 !== 0 ? S.chetNechet : ''}>
+            <li key={index} id={`hw04-task-${index}`}  className={index % 2 === 0 ? s.chetNechet : ''}>
               {el}
             </li>
           );
